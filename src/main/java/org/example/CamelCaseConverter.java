df4d96a2e92d614e6c3d0e20cf5839c393b5a568
Bruno Barros <contato4code@gmail.com>
2022-10-24 20:35:50 -0300

    testes simples

---
+++ src/main/java/org/example/CamelCaseConverter.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class CamelCaseConverter {
+    public String converter(String name) {
+
+        return name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+    }
+}
---
+++ src/test/java/junit/EntendendoJUnitTest.java
@@ -0,0 +1,30 @@
+package junit;
+
+import org.example.CamelCaseConverter;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EntendendoJUnitTest {
+    CamelCaseConverter camelCase;
+
+    @Before
+    public void setup() {
+        camelCase = new CamelCaseConverter();
+
+    }
+
+
+    @Test
+    public void aplicarCamelCaseEmNomeUnico() throws Exception {
+        assertEquals("Lionel", camelCase.converter("lionel"));
+    }
+
+    @Test
+    public void deveConverterNomeSimplesMisturadoMaiusculoEMinusculo() throws Exception {
+        assertEquals("Lionel", camelCase.converter("lIoNeL"));
+    }
+
+}
+
